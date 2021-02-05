2e914c053be2a6a92e65de6d2fc18d30da483bf4
adm.eremichev.i.a <diablero797@gmail.com>
2021-02-05 12:16:36 +0300

    Город:    Tula Температура:    -12 °C Ощущается как:    -19 °C Скорость ветра:    7 м/сек Влажность:    94 %

--- main.py
+++ main.py
@@ -16,45 +16,38 @@ bot = telebot.TeleBot(config.token)
 
 url = config.WEATHER_URL.format(city=config.location, token=config.WEATHER_TOKEN)  ### URL запроса
 print(url)
-response = requests.get(url)              ### Ответ на запрос
+response = requests.get(url)  ### Ответ на запрос
 # if response.status_code != 200:
 #     return 'city not found'
-data = json.loads(response.content)        ### Полученные данные
+data = json.loads(response.content)  ### Полученные данные
 
-data_temp = data['main']['temp'] - 273      ### Получение температуры из данных
-data_city = data['name']                   ### Получение города
-data_speed = data['wind']['speed']                   ### Получение города
+data_temp = data['main']['temp'] - 273
+data_feels_like_temp = data['main']['feels_like'] - 273
+data_city = data['name']
+data_speed = data['wind']['speed']
+data_humidity = data['main']['humidity']
 
 temp = str(math.ceil(data_temp))
+feels_like_temp = str(math.ceil(data_feels_like_temp))
 city = data_city
 speed = str(math.ceil(data_speed))
-
+humidity = str(data_humidity)
 
 print(data)
 
 print(data_city)
 print(temp)
+print(feels_like_temp)
 print(speed)
 
-
-# print('hello')
-
-
-# def parse_weather_data(data):
-#     for elem in data['weather']:
-#         weather_state = elem['main']
-#     temp = round(data['main']['temp'] - 273.15, 2)
-#     city = data['name']
-#     msg = f'The weather in {city}: Temp is {temp}, State is {weather_state}'
-#     return msg
-# print(msg)
 
 ######  Отправка сообщений
 @bot.message_handler(commands=['start', 'help'])
 def main(message):
-    bot.send_message(message.chat.id, 'Город:   ' + data_city + '\n' + 'Температура:    ' + temp + '\n' + 'Скорость '
-                                                                                                          'ветра:   '
-                     + speed + ' м/сек')
+    bot.send_message(message.chat.id,
+                     'Город:    ' + data_city + '\n' + 'Температура:    ' + temp + ' °C' + '\n' + 'Ощущается как:    '
+                     + feels_like_temp + ' °C' + '\n' + 'Скорость ветра:    ' + speed + ' м/сек' + '\n'
+                     + 'Влажность:    ' + humidity + ' %')
 
 
 if __name__ == '__main__':
