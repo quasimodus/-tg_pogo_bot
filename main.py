afd404e35011cbea4bcba2432b3e7f03aefd094a
quasimodus <diablero797@gmail.com>
2021-02-07 00:01:48 +0300

    city selection is working

--- main.py
+++ main.py
@@ -6,51 +6,63 @@ import requests
 
 bot = telebot.TeleBot(config.token)
 
-url = config.WEATHER_URL.format(city=config.location, token=config.WEATHER_TOKEN)  ### URL запроса
-print(url)
-response = requests.get(url)  ### Ответ на запрос
-# if response.status_code != 200:
-#     return 'city not found'
-data = json.loads(response.content)  ### Полученные данные
-
-data_temp = data['main']['temp'] - 273
-data_feels_like_temp = data['main']['feels_like'] - 273
-data_city = data['name']
-data_speed = data['wind']['speed']
-data_humidity = data['main']['humidity']
-description = data['weather'][0]['main']
-
-temp = str(math.ceil(data_temp))
-feels_like_temp = str(math.ceil(data_feels_like_temp))
-city = data_city
-speed = str(math.ceil(data_speed))
-humidity = str(data_humidity)
-
-print(data)
-
-print(data_city)
-print(temp)
-print(feels_like_temp)
-print(speed)
-print(description)
-
-if description == 'Sun':
-    description = 'Ясно ☀️☀️☀️'
-    print('Ясно')
-elif description == 'Clouds':
-    description = 'Облачно ☁️☁️☁️'
-    print('Облачно')
-elif description == 'Snow':
-    description = 'Снег ❄️❄️❄️'
-    print('Снег')
-
-    ######  Отправка сообщений
 
 
-@bot.message_handler(commands=['start', 'help'])
-def main(message):
+# @bot.message_handler(content_types=['text'])
+# def send_text(message):
+#     if message.text.lower() == 'привет':
+#         bot.send_message(message.chat.id, 'Привет, мой создатель')
+#     elif message.text.lower() == 'пока':
+#         bot.send_message(message.chat.id, 'Прощай, создатель')
+
+
+@bot.message_handler(content_types=['text'])
+def get_weather(message):
+    location = message.text.lower()
+    url = config.WEATHER_URL.format(city=location, token=config.WEATHER_TOKEN)  ### URL запроса
+    print(url)
+    response = requests.get(url)  ### Ответ на запрос
+    # if response.status_code != 200:
+    #     return 'city not found'
+    data = json.loads(response.content)  ### Полученные данные
+
+    data_temp = data['main']['temp'] - 273
+    data_feels_like_temp = data['main']['feels_like'] - 273
+    data_city = data['name']
+    data_speed = data['wind']['speed']
+    data_humidity = data['main']['humidity']
+    description = data['weather'][0]['main']
+
+    temp = str(math.ceil(data_temp))
+    feels_like_temp = str(math.ceil(data_feels_like_temp))
+    city = data_city
+    speed = str(math.ceil(data_speed))
+    humidity = str(data_humidity)
+
+    print(data)
+
+    print(data_city)
+    print(temp)
+    print(feels_like_temp)
+    print(speed)
+    print(description)
+
+    if description == 'Sun':
+        description = 'Ясно ☀️☀️☀️'
+        print('Ясно')
+    elif description == 'Clouds':
+        description = 'Облачно ☁️☁️☁️'
+        print('Облачно')
+    elif description == 'Snow':
+        description = 'Снег ❄️❄️❄️'
+        print('Снег')
+
+        ######  Отправка сообщений
+
+    # @bot.message_handler(commands=['start', 'help'])
+    # def main(message):
     bot.send_message(message.chat.id,
-                     '\n' + 'Город:    ' + data_city + '\n' + description + '\n' + 'Температура:    ' + temp
+                     '\n' + 'Город:    ' + city + '\n' + description + '\n' + 'Температура:    ' + temp
                      + ' °C' + '\n' + 'Ощущается как:    '
                      + feels_like_temp + ' °C' + '\n' + 'Скорость ветра:    ' + speed + ' м/сек' + '\n'
                      + 'Влажность:    ' + humidity + ' %' + '\n' + 'Источник: https://openweathermap.org',
